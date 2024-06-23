4d75b8620ce96eaca280aec28d26239750726c94
3dfish <wwj.joe@qq.com>
2024-06-23 12:55:18 +0000

    第一次提交

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();//函数express的返回值
+const fs = require('fs');
+const { exec } = require('child_process');
+const httpsOption = {
+//	key:fs.readFileSync('/etc/letsencrypt/archive/craws.cn/privkey1.pem'),
+//	cert:fs.readFileSync('/etc/letsencrypt/archive/craws.cn/fullchain1.pem')
+	key:fs.readFileSync('./https/key.pem'),
+	cert:fs.readFileSync('./https/cert.pem'),
+//	ca:fs.readFileSync('./https/craws.cn.csr')
+}
+
+servs = require('https').createServer(httpsOption,app);
+
+app.get('/factorio',function(req,res){
+	res.sendFile(__dirname + '/client/index.html');/////////////////////////////
+});
+app.get('/factorio/start',function(req,res){
+	exec('sudo systemctl start factorio.service', (error, stdout, stderr) => {
+	  if (error) {
+		console.error(`执行出错: ${error}`);
+		return;
+	  }
+	  if (stderr) {
+		console.error(`stderr: ${stderr}`);
+		return;
+	  }
+	  console.log(`stdout: ${stdout}`);
+	});
+});
+
+app.use('/etc',express.static(__dirname + '/client/etc'));
+
+servs.listen(6443);
